da4ee685f77ba765e9b497d5e3d8db2a035f4eb1
Arafullahi <arafullata@gmail.com>
2023-03-26 05:07:58 +0000

    adding docker file and compose file configuare the eureka sever
    adding entrypoint sh command
    
    adding the enrypoint.sh command and changes all docker files
    
    remove large files

--- user-service/src/main/java/com/dailycodebuffer/UserService/UserServiceApplication.java
+++ user-service/src/main/java/com/dailycodebuffer/UserService/UserServiceApplication.java
@@ -8,9 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
