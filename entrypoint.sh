945320b5efdf81bf444fa40bbce60ed036968869
Arafullahi <arafullata@gmail.com>
2023-04-02 08:57:17 +0000

    add api gateway

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # You can set here anything you need before starting the spring boot application
-mvn -s settings.xml spring-boot:run
+mvn -s settings.xml clean spring-boot:run -DskipTests=true
